b7d00b87708d4c5d5010776d129a03bf72e273b2
Rafael Lopes Ferrari <rafael.ferrari@nexxera.com>
2017-05-09 11:51:19 -0300

    Documentacao

--- src/main/java/com/campanha_core/domain/campanha/CampanhaRepository.java
+++ src/main/java/com/campanha_core/domain/campanha/CampanhaRepository.java
@@ -4,12 +4,37 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repositório de Campanha
+ * 
+ * @author rafael.ferrari
+ */
 public interface CampanhaRepository extends CrudRepository<Campanha, Long> {
 
+    /**
+     * Busca as Campanhas Vigentes.
+     * 
+     * @param date - Data corrente para filtro.
+     * @return As Campanhas se encontradas.
+     */
     List<Campanha> findByDataFinalGreaterThanEqual(final LocalDate date);
 
+    /**
+     * Busca as Campanhas por filtro de Data Inicial e Data Final.
+     * 
+     * @param dataInicial - Data Inicial da Campanha.
+     * @param dataFinal - Data Final da Campanha.
+     * @return As Campanhas se encontradas.
+     */
     List<Campanha> findByDataInicialGreaterThanEqualAndDataFinalLessThanEqual(final LocalDate dataInicial, final LocalDate dataFinal);
 
+    /**
+     * Busca as Campanhas Vigentes pelo ID do Time do Coração.
+     * 
+     * @param date - Data Corrente para filtro.
+     * @param idTimeCoracao - ID do Time do Coração.
+     * @return As Campanhas se encontradas.
+     */
     List<Campanha> findByDataFinalGreaterThanEqualAndIdTimeCoracao(final LocalDate date, final Long idTimeCoracao);
 
 }
--- src/main/java/com/campanha_core/domain/campanha/CampanhaService.java
+++ src/main/java/com/campanha_core/domain/campanha/CampanhaService.java
@@ -3,16 +3,54 @@ package com.campanha_core.domain.campanha;
 import com.campanha_core.domain.exceptions.ServiceException;
 import java.util.Optional;
 
+/**
+ * Service de Campanha.
+ * 
+ * @author rafael.ferrari
+ */
 public interface CampanhaService {
 
+    /**
+     * Busca todas as Campanhas vigentes.
+     * 
+     * @return As Campanhas se encontradas.
+     * @throws ServiceException .
+     */
     Iterable<Campanha> findAll() throws ServiceException;
 
+    /**
+     * Busca todas as Campanhas vigentes pelo ID do Time do Coração.
+     * 
+     * @param idTimeCoracao - ID do Time do Coração à ser pesquisado.
+     * @return As Campanhas se encontrado.
+     * @throws ServiceException .
+     */
     Iterable<Campanha> findAll(final Long idTimeCoracao) throws ServiceException;
 
+    /**
+     * Busca uma Campanha pelo ID.
+     * 
+     * @param id - ID da Campanha à ser pesquisada.
+     * @return Campanha se encontrada.
+     * @throws ServiceException .
+     */
     Optional<Campanha> findOne(final Long id) throws ServiceException;
 
+    /**
+     * Salva/Atualiza uma Campanha.
+     * 
+     * @param campanha - Campanha à ser Salva/Atualizada.
+     * @return Campanha.
+     * @throws ServiceException .
+     */
     Optional<Campanha> save(final Campanha campanha) throws ServiceException;
 
+    /**
+     * Remove uma campanha pelo ID.
+     * 
+     * @param id - ID da Campanha à ser removida.
+     * @throws ServiceException .
+     */
     void delete(final Long id) throws ServiceException;
 
 }
